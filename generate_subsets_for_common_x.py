252f2830f7acdee97b2397d8e16adf5dfeb43eb4
Mironovich Svetlana <mazukta26@gmail.com>
2016-04-24 00:47:38 +0300

    some progress in feature extending realization

--- generate_subsets_for_common_x.py
+++ generate_subsets_for_common_x.py
@@ -1,4 +1,5 @@
 import time
+import sys
 from os.path import isfile
 import numpy as np
 import pandas as pd
@@ -15,11 +16,12 @@ def make_new_generator():
     X = get_data_keeper().get_common_x()
     print "matrix shape before:", X.shape
     X[X!=1] = 0
-    to_drop = (X.sum(axis=0) >= (X.shape[0] / 2)) | (X.sum(axis=0) == 0)
+    to_drop = (X.sum(axis=0) >= (X.shape[0] / 2)) | (X.sum(axis=0) < 3)
     to_drop = to_drop[to_drop].index
     X = X.drop(to_drop, axis=1)
     X.to_csv(RAW_X_BEFORE_SUBSETE_GENERATION_PATH)
     print "matrix shape after:", X.shape
+    sys.stdout.flush()
     generator = SubsetGenerator()
     generator.generate_and_set(X.as_matrix().astype(np.uint8))
     print "generating done, time from start spent:", time.time() - start_time
