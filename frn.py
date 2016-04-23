7d77df47e8e028dcbe1b112449fe577aa98e9aad
Mironovich Svetlana <mazukta26@gmail.com>
2016-04-23 22:22:46 +0300

    frn debugged

--- frn.py
+++ frn.py
@@ -7,7 +7,7 @@ from common import forward_out
 
 mem_frn = Memory(cachedir='cache/frn')
 
-@forward_out("logs/frn.log")
+#@forward_out("logs/frn.log")
 @mem_frn.cache
 def get_rel_feat_frn(params, X, feature_importances, importance_threshold):
     frn = FeatureRelevanceNetwork(**params)
@@ -26,7 +26,7 @@ class FeatureRelevanceNetwork(BaseEstimator):
     def _get_model_parameters(self, X, feature_importances, importance_threshold):
         real_importances = (np.array(feature_importances) - np.array(importance_threshold)).astype(np.float64)
         real_importances /= np.sqrt((real_importances ** 2).mean())
-        #print "real importances", (real_importances > 0).sum()
+        print "real importances", (real_importances > 0).sum()
         unary_cost = np.zeros((X.shape[1], 2), dtype=np.float64)
         unary_cost[:,0] = real_importances * self.feature_importance_priority
         pairwise_cost = np.array([[0.0, 1.0],
@@ -47,9 +47,12 @@ class FeatureRelevanceNetwork(BaseEstimator):
         return edges, edge_weights, unary_cost, pairwise_cost
 
     def get_relevant_features(self, X, feature_importances, importance_threshold):
+        print "get_relevant_features_call"
         edges, edge_weights, unary_cost, pairwise_cost = self._get_model_parameters(X, feature_importances, importance_threshold)
-        return pygco.cut_general_graph(edges, edge_weights, unary_cost, pairwise_cost, n_iter=1, algorithm="expansion")
-
+        result = pygco.cut_general_graph(edges, edge_weights, unary_cost, pairwise_cost, n_iter=1, algorithm="expansion")
+        print "features count:", result.sum()
+        print result, result.shape
+        return result
 
 
 
@@ -73,14 +76,16 @@ class FeatureRelevanceNetworkWrapper(BaseEstimator):
         return result
 
     def _filter_X(self, X):
-        #print type(X)
-        return np.array(X)[:,self._relevant_feature_mask]
+        print type(X)
+        result = np.array(X)[:,self._relevant_feature_mask.astype(np.bool)]
+        print "shape:", result.shape
+        return result
 
     def _set_relevant_feature_mask(self, X, y):
         feature_importances = self.feature_importances_getter.get_feature_importances(X, y)
-        #print feature_importances[:10]
+        print feature_importances[:10]
         importance_threshold = feature_importances.mean() * self.feature_selection_threshold_coef
-        #print feature_importances.mean(), importance_threshold, self.feature_selection_threshold_coef
+        print feature_importances.mean(), importance_threshold, self.feature_selection_threshold_coef, importance_threshold.sum()
         self._relevant_feature_mask = get_rel_feat_frn(self._get_reduced_params(), X, feature_importances, importance_threshold)
 
     def get_support(self, indices=False):
@@ -98,3 +103,4 @@ class FeatureRelevanceNetworkWrapper(BaseEstimator):
     def predict(self, X):
         X = self._filter_X(X)
         return self.inner_model.predict(X)
+
